d0aca00a6666591b6c9f75baddb78d89f817c77d
Albinator3000 <albert.w.opher.iv@gmail.com>
2025-06-17 15:04:36 -0700

    cleint and user

---
+++ user/userdetails.js
@@ -0,0 +1,19 @@
+/**
+ * @type {IAM User Details}
+ */
+
+
+/**
+ * @name test
+ * @var {Access Key}: AKIATVCCN7ZQ6HUHRUGF
+ * @var {Secret Access Key}: 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
+ */
+
+/**
+ * @name admin
+ * @var {Access Key}: AKIATVCCN7ZQSAIL4L7F
+ * @var {Secret Access Key}: 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
+ */
+
+
+
